31a135e94a200393074e8596b27dc0ce488c788d
Akhilan <akhilan.e0219045@sret.edu.in>
2025-07-31 12:31:30 +0530

    mongodb connection is done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ dotenv.config()
 
 const app = express()
 
+// connect DB
+const connectDB = require('./config/db')
+connectDB()
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log("Server running on PORT :",PORT)
